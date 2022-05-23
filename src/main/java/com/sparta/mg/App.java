560cdacbdd57ea0fdf0ebf231c5af4911e7365b1
vanisampath <vsampath@spartaglobal.com>
2022-05-23 15:16:38 +0100

    Initial commit

---
+++ src/main/java/com/sparta/mg/App.java
@@ -0,0 +1,32 @@
+package com.sparta.mg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int timeofDay = 21;
+       String welcomeMessage =  greeting(timeofDay);
+        System.out.println(welcomeMessage);
+    }
+
+    public static String greeting(int timeofDay) {
+        String message = "";
+        if (timeofDay > 0 && timeofDay <= 12){
+            message = "Good Morning";
+        }else if (timeofDay > 12 && timeofDay <= 18){
+            message = "Good Afternoon";
+        } else if (timeofDay > 18 && timeofDay <= 20){
+            message = "Good Evening";
+        } else if (timeofDay > 20 && timeofDay < 24 ) {
+            message = "Good Night";
+        }else{
+            message = "Not a valid time";
+        }
+
+        return message;
+    }
+}
---
+++ src/test/java/com/sparta/mg/apptest.java
@@ -0,0 +1,57 @@
+package com.sparta.mg;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class apptest {
+    @Test
+    @DisplayName("check that 5 == 5")
+    void simpleTest() {
+        Assertions.assertEquals(true, 5 == 5);
+    }
+
+    @Test
+    @DisplayName("check that we can return Good Afternoon")
+    void checkWeCanReturnGoodAfternoon(){
+        Assertions.assertEquals("Good Afternoon",App.greeting(13));
+    }
+
+    @Test
+    @DisplayName("check that we can return Good Evening")
+    void checkWeCanReturnGoodEvening(){
+        Assertions.assertEquals("Good Evening",App.greeting(19));
+    }
+
+    @Test
+    @DisplayName("check that we can return Good Night")
+    void checkWeCanReturnGoodNight(){
+        Assertions.assertEquals("Good Night",App.greeting(21));
+    }
+    @Test
+    @DisplayName("check that we can return Invalid Time")
+    void checkWeCanReturnInvalidTime(){
+        Assertions.assertEquals("Not a valid time",App.greeting(26));
+    }
+    @Test
+    @DisplayName("check that we can return Invalid Time")
+    void checkWeCanReturnInvalidTimeNegative() {
+        Assertions.assertEquals("Not a valid time", App.greeting(-1));
+    }
+    @Test
+    @DisplayName("check that we can return Boundary values")
+    void checkWeCanReturnBoundaryMorning(){
+        Assertions.assertEquals("Good Morning",App.greeting(12));
+    }
+
+    @Test
+    @DisplayName("check that we can return Boundary values")
+    void checkWeCanReturnBoundaryEvening(){
+        Assertions.assertEquals("Good Evening",App.greeting(20));
+    }
+    @Test
+    @DisplayName("check that we can return Boundary values")
+    void checkWeCanReturnBoundaryNight(){
+        Assertions.assertEquals("Good Night",App.greeting(23));
+    }
+}
